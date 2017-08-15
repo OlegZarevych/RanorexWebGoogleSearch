04948bd4805f80684ac909a7f88d27b6ab89c954
Oleg <Oleg.Zarevych@gmail.com>
2017-08-16 00:06:39 +0300

    add new pages

---
+++ RanorexWebGoogleSearch/Pages/PasswdInPage.cs
@@ -0,0 +1,35 @@
+﻿using Ranorex;
+
+namespace RanorexWebGoogleSearch.Pages
+{
+    class PasswdInPage
+    {
+        private const string _pwdInput = "/dom[@domain='accounts.google.com']//input[@name='password']";
+        private const string _HeadName = "/dom[@domain='accounts.google.com']//h1[#'headingText']";
+        private const string _nextBtn = "/dom[@domain='accounts.google.com']//div[#'passwordNext']";
+
+        public WebElement PassInput
+        {
+            get
+            {
+                WebElement pwdInput = _pwdInput;
+                return pwdInput;
+            }
+        }
+
+        public WebElement NextBtn
+        {
+            get
+            {
+                WebElement next = _nextBtn;
+                return next;
+            }
+        }
+
+        public void EnterPassAndNext(string pass)
+        {
+            PassInput.PressKeys(pass);
+            NextBtn.Click();
+        }
+    }
+}
--- RanorexWebGoogleSearch/Pages/SignInPage.cs
+++ RanorexWebGoogleSearch/Pages/SignInPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ranorex;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace RanorexWebGoogleSearch.Pages
 {
     class SignInPage
     {
+        private const string _usernameInput = "/dom[@domain='accounts.google.com']//input[#'identifierId']";
+        private const string _nextBtn = "/dom[@domain='accounts.google.com']//div[#'identifierNext']";
+
+        public static WebElement UserName
+        {
+            get
+            {
+                WebElement userName = _usernameInput;
+                return userName;
+            }
+        }
+
+        public static WebElement NextBtn
+        {
+            get
+            {
+                WebElement nextBtn = _nextBtn;
+                return nextBtn;
+            }
+        }
+
+        public PasswdInPage EnterUserNameAndNext(string name)
+        {
+            UserName.PressKeys(name);
+            NextBtn.Click();
+            return new PasswdInPage();
+        }
     }
 }
