a2622f520b8de23d8a51ff74b5d8f3c4252f66fe
Oleg <Oleg.Zarevych@gmail.com>
2017-08-16 00:34:01 +0300

    Add new test class

---
+++ RanorexWebGoogleSearch/Tests/GoogleLogin.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RanorexWebGoogleSearch.Pages;
+using Ranorex;
+
+namespace RanorexWebGoogleSearch.Tests
+{
+    [TestFixture]
+    class GoogleLogin
+    {
+        [Test]
+        public void SignInPagePresent()
+        {
+            MainPage MP = new MainPage();
+            SignInPage p = MP.OpenSignInPage();
+            Assert.That(SignInPage.UserName.Visible, Is.True);
+        }
+    }
+}
